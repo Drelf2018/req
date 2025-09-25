3684fd19976dd4586e943a78814583df7d1e3ac3
12138 <41439182+Drelf2018@users.noreply.github.com>
2025-09-25 16:16:55 +0800

    feat: 为 StructCookieJar 添加 JSON 序列化支持

--- cookie.go
+++ cookie.go
@@ -1,6 +1,9 @@
 package req
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/url"
 	"reflect"
@@ -19,10 +22,10 @@ type StructCookieJar[T any] struct {
 	//		UID      string `json:"uid"`
 	//		Username string `cookie:"username"`
 	//	}]
-	C            T
-	rw           sync.RWMutex
-	ptr          unsafe.Pointer
-	fieldOffsets map[string]uintptr
+	C       T
+	rw      sync.RWMutex
+	ptr     unsafe.Pointer
+	offsets map[string]uintptr
 }
 
 // 计算所有可作为 Cookie 字段的偏移量
@@ -39,11 +42,13 @@ func (s *StructCookieJar[T]) init(t reflect.Type, parentOffset uintptr) {
 				if exists {
 					name = strings.Split(name, ",")[0]
 				}
+			} else if name == "-" {
+				continue
 			}
 			if !exists {
 				name = field.Name
 			}
-			s.fieldOffsets[name] = parentOffset + field.Offset
+			s.offsets[name] = parentOffset + field.Offset
 		} else if field.Type.Kind() == reflect.Struct {
 			s.init(field.Type, parentOffset+field.Offset)
 		}
@@ -54,13 +59,13 @@ func (s *StructCookieJar[T]) init(t reflect.Type, parentOffset uintptr) {
 func (s *StructCookieJar[T]) SetCookies(u *url.URL, cookies []*http.Cookie) {
 	s.rw.Lock()
 	defer s.rw.Unlock()
-	if s.fieldOffsets == nil {
+	if s.offsets == nil {
 		s.ptr = unsafe.Pointer(s)
-		s.fieldOffsets = make(map[string]uintptr)
+		s.offsets = make(map[string]uintptr)
 		s.init(reflect.TypeOf(s.C), unsafe.Offsetof(s.C))
 	}
 	for _, cookie := range cookies {
-		offset, exists := s.fieldOffsets[cookie.Name]
+		offset, exists := s.offsets[cookie.Name]
 		if exists {
 			*(*string)(unsafe.Add(s.ptr, offset)) = cookie.Value
 		}
@@ -69,31 +74,68 @@ func (s *StructCookieJar[T]) SetCookies(u *url.URL, cookies []*http.Cookie) {
 
 // 返回所有已设置的 Cookie 切片，不会判断是否与 u 同源
 func (s *StructCookieJar[T]) Cookies(u *url.URL) []*http.Cookie {
-	if s.fieldOffsets == nil {
+	if s.offsets == nil {
 		return nil
 	}
 	s.rw.RLock()
 	defer s.rw.RUnlock()
-	cookies := make([]*http.Cookie, 0, len(s.fieldOffsets))
-	for name, offset := range s.fieldOffsets {
+	cookies := make([]*http.Cookie, 0, len(s.offsets))
+	for name, offset := range s.offsets {
 		cookies = append(cookies, &http.Cookie{Name: name, Value: *(*string)(unsafe.Add(s.ptr, offset))})
 	}
 	return cookies
 }
 
+var _ http.CookieJar = (*StructCookieJar[any])(nil)
+
 // 返回指定名称的 Cookie
 func (s *StructCookieJar[T]) Get(name string) (cookie *http.Cookie, exists bool) {
-	if s.fieldOffsets == nil {
+	if s.offsets == nil {
 		return nil, false
 	}
 	s.rw.RLock()
 	defer s.rw.RUnlock()
 	var offset uintptr
-	offset, exists = s.fieldOffsets[name]
+	offset, exists = s.offsets[name]
 	if exists {
 		cookie = &http.Cookie{Name: name, Value: *(*string)(unsafe.Add(s.ptr, offset))}
 	}
 	return
 }
 
-var _ http.CookieJar = (*StructCookieJar[any])(nil)
+func (s *StructCookieJar[T]) MarshalJSON() ([]byte, error) {
+	if s.offsets == nil {
+		return nil, nil
+	}
+	s.rw.RLock()
+	defer s.rw.RUnlock()
+	first := true
+	buf := bytes.NewBuffer([]byte{'{'})
+	for name, offset := range s.offsets {
+		if !first {
+			buf.WriteByte(',')
+		}
+		first = false
+		buf.WriteString(`"` + name + `":"` + *(*string)(unsafe.Add(s.ptr, offset)) + `"`)
+	}
+	buf.WriteByte('}')
+	return buf.Bytes(), nil
+}
+
+var _ json.Marshaler = (*StructCookieJar[any])(nil)
+
+func (s *StructCookieJar[T]) UnmarshalJSON(data []byte) error {
+	m := make(map[string]string)
+	err := json.Unmarshal(data, &m)
+	if err != nil {
+		return fmt.Errorf("req: unmarshal StructCookieJar failed: %w", err)
+	}
+	cookies := make([]*http.Cookie, 0, len(m))
+	for name, value := range m {
+		cookies = append(cookies, &http.Cookie{Name: name, Value: value})
+	}
+	s.SetCookies(nil, cookies)
+	return nil
+}
+
+var _ json.Unmarshaler = (*StructCookieJar[any])(nil)
