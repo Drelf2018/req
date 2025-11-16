e2649a7b6da24a4949542dfb9099e5fceb694815
12138 <drelf2018@outlook.com>
2025-11-17 02:16:35 +0800

    remove: 移除旧版代码

--- interfaces.go
+++ interfaces.go
@@ -1,10 +1,7 @@
 package req
 
 import (
-	"io"
 	"net/http"
-	"net/url"
-	"reflect"
 	"time"
 )
 
@@ -14,36 +11,20 @@ type API interface {
 	RawURL() string
 }
 
-// API 请求体
-type APIBody interface {
-	Body(cli *Client, body []Field, value reflect.Value, api API) (io.Reader, error)
+// 请求前钩子
+type BeforeRequest interface {
+	BeforeRequest(cli *http.Client, req *http.Request, api API) error
 }
 
-// API 请求参数
-type APIQuery interface {
-	Query(req *http.Request, cli *Client, query []Field, value reflect.Value, api API) error
+// 检验响应
+type CheckResponse interface {
+	CheckResponse(cli *http.Client, resp *http.Response, api API) error
 }
 
-// API 请求头
-type APIHeader interface {
-	Header(req *http.Request, cli *Client, header []Field, value reflect.Value, api API) error
+// 可解包出错误的接口返回值
+type Unwrap interface {
+	Unwrap() error
 }
-
-// 可添加接口
-type Adder interface {
-	Add(string, string)
-}
-
-var _ Adder = (*url.Values)(nil)
-var _ Adder = (*http.Header)(nil)
-
-type FuncAdder func(string, string)
-
-func (f FuncAdder) Add(key, val string) {
-	f(key, val)
-}
-
-var _ Adder = FuncAdder(nil)
 
 // 重试计时器
 type RetryTicker interface {
@@ -55,19 +36,4 @@ type RetryTicker interface {
 	//
 	// 返回值 ok 表示是否继续重试
 	NextRetry(retried int) (delay time.Duration, ok bool)
-}
-
-// 请求前钩子
-type BeforeRequest interface {
-	BeforeRequest(req *http.Request, cli *http.Client, api API, retried int)
-}
-
-// 检验响应
-type CheckResponse interface {
-	CheckResponse(resp *http.Response, cli *http.Client, api API, retried int) error
-}
-
-// 可解包出错误的接口返回值
-type Unwrap interface {
-	Unwrap() error
 }
