d238b65f204ed1511432b255f226d89282723136
12138 <drelf2018@outlook.com>
2025-11-17 02:13:34 +0800

    fix!: 修改了自定义 Context 的取值逻辑

--- value.go
+++ value.go
@@ -10,14 +10,15 @@ type mapCtx struct {
 	m map[string]any
 }
 
-// Value 首先尝试从当前 mapCtx 的映射中查找键对应的值，如果未找到，则委托给父上下文的 Value 方法
+// Value 首先尝试从父上下文的 Value 方法获取值，如果未找到，则从当前 mapCtx 的映射中查找键对应的值
 func (c *mapCtx) Value(key any) any {
-	if k, ok := key.(string); ok {
-		if v, ok := c.m[k]; ok {
-			return v
-		}
+	if v := c.Context.Value(key); v != nil {
+		return v
 	}
-	return c.Context.Value(key)
+	if k, ok := key.(string); ok {
+		return c.m[k]
+	}
+	return nil
 }
 
 // WithMap 创建一个新上下文，用于存储和获取额外的上下文值
@@ -25,9 +26,11 @@ func (c *mapCtx) Value(key any) any {
 // parent 为新上下文的父上下文，不能为空
 //
 // m 为要附加的键值对映射，会被复制到新的上下文中，后续对 m 的修改不会影响上下文中的值
+//
+// Value 首先尝试从父上下文的 Value 方法获取值，如果未找到，则从当前 mapCtx 的映射中查找键对应的值
 func WithMap(parent context.Context, m map[string]any) context.Context {
 	if parent == nil {
-		panic("cannot create context from nil parent")
+		panic("req.WithMap: cannot create context from nil parent")
 	}
 	ctx := &mapCtx{parent, make(map[string]any, len(m))}
 	for k, v := range m {
@@ -60,9 +63,11 @@ func (c *valuesCtx) Value(key any) any {
 // parent 为新上下文的父上下文，不能为空
 //
 // values 为要附加的子上下文列表
+//
+// Value 首先从父上下文中查找键对应的值，如果未找到，则依次从子上下文中查找，返回第一个找到的值
 func WithValues(parent context.Context, values ...context.Context) context.Context {
 	if parent == nil {
-		panic("cannot create context from nil parent")
+		panic("req.WithValues: cannot create context from nil parent")
 	}
 	filtered := make([]context.Context, 0, len(values))
 	for _, ctx := range values {
