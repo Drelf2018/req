9400307d56e36768bc62a0e4bff4ec8b10472c83
12138 <41439182+Drelf2018@users.noreply.github.com>
2025-11-28 19:44:19 +0800

    fix: 将内置命名替换函数变量改为全局函数

--- method/replacer.go
+++ method/replacer.go
@@ -5,24 +5,13 @@ import (
 	"unicode"
 )
 
-// HeaderReplacer 将字符串中的大写字母替换为横杠加大写字母，大写首字母前不添加横杠，连续的大写字母只在第一个字母前添加横杠
-var HeaderReplacer = func(s string) string {
-	if s == "" {
-		return ""
-	}
-	var result strings.Builder
-	for i, r := range s {
-		// 本身是大写字母，不是第一个字符并且前一个不是大写字母，添加横杠
-		if unicode.IsUpper(r) && i > 0 && !unicode.IsUpper(rune(s[i-1])) {
-			result.WriteRune('-')
-		}
-		result.WriteRune(r)
-	}
-	return result.String()
-}
+var (
+	NameReplacer   = CamelToSnake
+	HeaderReplacer = CamelToHyphenated
+)
 
-// NameReplacer 将字符串中的大写字母替换为下划线加小写字母，大写首字母前不添加下划线，连续的大写字母只在第一个字母前添加下划线
-var NameReplacer = func(s string) string {
+// CamelToSnake 将字符串中的大写字母替换为下划线加小写字母，大写首字母前不添加下划线，连续的大写字母只在第一个字母前添加下划线
+func CamelToSnake(s string) string {
 	if s == "" {
 		return ""
 	}
@@ -39,6 +28,22 @@ var NameReplacer = func(s string) string {
 			// 非大写字母直接添加
 			result.WriteRune(r)
 		}
+	}
+	return result.String()
+}
+
+// CamelToHyphenated 将字符串中的大写字母替换为横杠加大写字母，大写首字母前不添加横杠，连续的大写字母只在第一个字母前添加横杠
+func CamelToHyphenated(s string) string {
+	if s == "" {
+		return ""
+	}
+	var result strings.Builder
+	for i, r := range s {
+		// 本身是大写字母，不是第一个字符并且前一个不是大写字母，添加横杠
+		if unicode.IsUpper(r) && i > 0 && !unicode.IsUpper(rune(s[i-1])) {
+			result.WriteRune('-')
+		}
+		result.WriteRune(r)
 	}
 	return result.String()
 }
